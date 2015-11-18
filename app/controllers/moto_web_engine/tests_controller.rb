f5f5d8bee2da1d2e58b24c374147d2ec5b370c1d
Bartek Wilczek <bwilczek@gmail.com>
2015-11-18 16:02:30 +0100

    General development of different views

--- app/controllers/moto_web_engine/tests_controller.rb
+++ app/controllers/moto_web_engine/tests_controller.rb
@@ -3,8 +3,9 @@ require_dependency "moto_web_engine/application_controller"
 module MotoWebEngine
   class TestsController < ApplicationController
     
-    def index
-      @tests = Test.last 10
+    def show
+      @test = Test.find params[:id]
+      render layout: 'moto_web_engine/no_menu'
     end
 
   end
--- app/helpers/moto_web_engine/application_helper.rb
+++ app/helpers/moto_web_engine/application_helper.rb
@@ -1,4 +1,34 @@
 module MotoWebEngine
   module ApplicationHelper
+    
+    def bgcolor_result(result)
+      case result.to_sym
+      when :pending
+        '#ffeeee'
+      when :running
+        '#ffcc00'
+      when :passed
+        '#00ff00'
+      when :failure
+        '#ff0000'
+      when :error
+        '#cc0099'
+      when :skipped
+        '#0066ff'
+      else
+        '#ffffff'
+      end     
+    end
+    
+    def duration(d)
+      return '-' if d.nil?
+      secs = d.to_i % 60
+      mins = d.to_i / 60
+      fraction = (d - d.to_i).round(3).to_s[2..-1]
+      return "0.#{fraction}\"" if mins == 0 && secs == 0
+      return "#{secs}.#{fraction}\"" if mins == 0
+      "#{mins}'#{secs}.#{fraction}\""
+    end
+          
   end
 end
